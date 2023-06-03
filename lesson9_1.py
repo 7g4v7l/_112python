60867bf9f4d52b7bf9a51b32e790ef480223665f
7g4v7l <wann4868@gmail.com>
2023-06-03 10:37:27 +0800

    modify

--- lesson9_1.py
+++ lesson9_1.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
-st.write("## Hello world Robert")
+"## Hello world Robert"
 st.write("Hello ***Charles Wann***")
+
+st.markdown("Hello! **world**!")
+st.divider()
+st.title("這是app的title")
+st.divider()
+st.header("這是header")
+st.divider()
+st.subheader("這是subheader")
+st.divider()
+st.caption("這是caption")
+st.divider()
+st.code("a=123")
