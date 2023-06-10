7af1ab8a3cc2881c9b4bec306b7932d5bc79e8c9
7g4v7l <132725535+7g4v7l@users.noreply.github.com>
2023-06-10 06:52:22 +0000

    modify

--- lesson12_1.py
+++ lesson12_1.py
@@ -5,4 +5,4 @@ import pandas as pd
 codeFrame = pd.read_csv('codeSearch.csv',usecols=['code','name'])
 codeSeries=codeFrame['code'].astype(str) + codeFrame['name']
 with st.sidebar:
-    st.write("請選擇股票代碼:",codeSeries)
+    st.multiselect("請選擇股票:",codeSeries)("請選擇股票代碼:",codeSeries)
